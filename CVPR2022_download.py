2bb210d3dc937aa8cb0da7ffdbe6d60846f425f0
Raymond Zuo <zfr900301@gmail.com>
2023-04-06 10:18:04 +0800

    Create CVPR2022_download.py

---
+++ CVPR2022_download.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# coding:utf-8
+
+import urllib
+import re
+
+def getHtmlContent(url):
+    page = urllib.urlopen(url)
+    return page.read()
+
+def getBlock(html):
+    re_block = r"<dt class=\"ptitle\"><br><a(.*?)</dd>\n<dt"
+    re_title = r'html">(.*?)</a></dt>'
+    re_paper = r'href="(.*?)">pdf<'
+    re_supp = r'href="(.*?)">supp<'
+    block = re.compile(re_block, re.S)
+    blocks = re.findall(block, html)
+    msg = []
+    for temp in blocks:
+        title = re.findall(re.compile(re_title, re.S), temp)[0].replace('/', ' ')
+        title = title.replace(':', '')
+        title = title.replace(',', '')
+	paper_url = re.findall(re.compile(re_paper, re.S), temp)[0]
+        paper = 'https://openaccess.thecvf.com/'+paper_url
+        if re.findall(re.compile(re_supp, re.S), temp):
+            supp = 'https://openaccess.thecvf.com/'+re.findall(re.compile(re_supp, re.S), temp)[0]
+        else:
+            supp = ''
+        sub_msg=[]
+        sub_msg.append(title)
+        sub_msg.append(paper)
+        sub_msg.append(supp)
+        msg.append(sub_msg)
+    return msg
+
+def download(url, fileName):
+    urllib.urlretrieve(url, fileName)
+
+def batchDownload(blocks, path):
+    count = 1
+    for block in blocks:
+        download(block[1], ''.join([path, '{0}.pdf'.format(block[0])]))
+        if block[2]:
+            download(block[2], ''.join([path, '{0}.pdf'.format(block[0]+'(supp)')]))
+        print 'downloading No.'+str(count)+'/'+str(len(blocks))+': '+block[0]
+        count = count + 1
+
+url = 'https://openaccess.thecvf.com/CVPR2022?day=all'
+html = getHtmlContent(url)
+blocks = getBlock(html)
+batchDownload(blocks, './')
